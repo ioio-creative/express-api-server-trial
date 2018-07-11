244ecb603b12487af80e99d9f49e8364a8d0f9b5
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-07-11 12:49:11 +0800

    added test api endpoint

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');  // example
+
+const defaultPort = 8080;
+
+/*
+  Important:
+  calling path.join() in Windows would result in '\' but not '/',
+  not desired for constructing api end points.
+*/
+const apiEndPointPrefix = '/api/';
+
+const app = express();
+
+// How to retrieve POST query parameters?
+// https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
+// https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// Put all API endpoints under '/api'
+// example
+// GET /api/passwords
+app.get(apiEndPointPrefix + 'passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+// test
+// POST /api/test
+app.post(apiEndPointPrefix + 'test', (req, res) => {
+  const reqBody = req.body;
+  console.log('Resequest received:');
+  console.log(reqBody);
+  console.log(`Button ${reqBody.btnPressed} is pressed.`);
+  res.json({
+    message: `Request: ${JSON.stringify(reqBody)} received`,
+    buttonPressed: reqBody.btnPressed
+  });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above
+// app.get('*', (req, res) => {
+//   res.status(404).send('Not found');
+// });
+
+const port = process.env.PORT || defaultPort;
+app.listen(port);
+
+console.log(`express-api-server-trial listening on ${port}`);
